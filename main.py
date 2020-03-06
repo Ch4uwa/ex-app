0ca57ec080380acb4a1c46e6362e0cbf952f9356
Martin K <mrtn.karlsson@gmail.com>
2020-03-06 17:16:08 +0100

    removed venv

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import sys
 
-from PySide2.QtWidgets import QMainWindow, QApplication
+from PySide2.QtCore import Qt, Slot
+from PySide2.QtWidgets import QMainWindow, QApplication, QAction
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__(self)
+        super(MainWindow, self).__init__()
+
+        self.action_quit = QAction('&Quit')
+
+        self.menu = self.menuBar()
+        self.main_menu = self.menu.addMenu('&Menu')
+        self.main_menu.addAction(self.action_quit)
+
+        self.action_quit.setShortcut('Ctrl+Q')
+
+        self.action_quit.triggered.connect(self.close_application)
+
+    @Slot()
+    def close_application(self):
+        sys.exit()
 
 
 def run():
