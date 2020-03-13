c123b4f7c5ffa9ddd6add7d642d1af18490f28dd
Martin K <mrtn.karlsson@gmail.com>
2020-03-13 11:19:58 +0100

    weather api start

--- database.py
+++ database.py
@@ -5,6 +5,16 @@ Email: mrtn.karlsson@gmail.com
 
 Handling database actions
 """
+import sys
+import os
+
+# Database might change
+import sqlite3
+
+
+class DataBaseActions:
+    def __init__(self):
+        super().__init__()
 
 
 if __name__ == '__main__':
--- request.py
+++ request.py
@@ -4,12 +4,43 @@ Email: mrtn.karlsson@gmail.com
 """
 # This file handles requests
 
-
 import os
 import sys
 
-from dotenv import load_dotenv
+# Todo Move .env file into db
+try:
+    import requests
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError as import_error:
+    print(import_error)
+
+
+# Class for handling weather API calls
+# Todo connect with selection of current location
+class Weather:
+    def __init__(self):
+        super().__init__()
+        # 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}'
+        self.appid = os.getenv('WEATHER_API')
+        self.q = 'Stockholm'
+        self.api_call = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(
+            self.q, self.appid)
+        print(self.api_call)
+
+    def get_weather(self):
+
+        try:
+            response = requests.get(self.api_call)
+        except Exception as e:
+            print('Exception:', e)
+        finally:
+            if response.ok:
+                print('Status: {}\nResponse:\n{}'.format(
+                    response.status_code, response.text))
 
 
 if __name__ == '__main__':
-    pass
+    print(__name__)
+    weather = Weather()
+    weather.get_weather()
