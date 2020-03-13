51436d2ae46d3624d2f92bfbd1a15b4583805fd3
Martin K <mrtn.karlsson@gmail.com>
2020-03-13 11:19:58 +0100

    weather api start

--- database.py
+++ database.py
@@ -5,6 +5,15 @@ Email: mrtn.karlsson@gmail.com
 
 Handling database actions
 """
+import sys
+import os
+
+import sqlite3
+
+
+class DataBaseActions:
+    def __init__(self):
+        super().__init__()
 
 
 if __name__ == '__main__':
