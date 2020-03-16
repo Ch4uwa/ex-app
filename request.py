e8a98ac8eacef4da2e893807b4a40c2802345a1c
Martin K <mrtn.karlsson@gmail.com>
2020-03-16 12:32:39 +0100

    moved imports in request.py into a try except

--- request.py
+++ request.py
@@ -11,7 +11,6 @@ import sys
 try:
     import requests
     from dotenv import load_dotenv
-    load_dotenv()
 
 except ImportError as import_error:
     # Expected
@@ -20,34 +19,38 @@ except Exception as imp_exception:
     # Unexpected
     print(imp_exception, False)
     print(imp_exception.__class__.__name__ + ': {}'.format(imp_exception))
+else:
+    load_dotenv()
 
 
 # Class for handling weather API calls
 # Todo connect with select current location
 class Weather:
-    def __init__(self):
-        super().__init__()
-        # 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}'
+    def __init__(self, city='Stockholm'):
         self.api_key = os.getenv('WEATHER_API')
-        self.city = 'Stockholm'
+        self.city = city
+
+        # 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}'
         self.api_call = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(
             self.city, self.api_key)
-        print(self.api_call)
 
     def get_weather(self):
         # Current weather Stockholm
         try:
             response = requests.get(self.api_call)
-            if response.ok:
-                print('Status Code: {}\nResponse:\n{}'.format(response.status_code, response.json()))
         except requests.exceptions as req_error:
             print('Exception:', req_error)
+        else:
+            print('Status Code: {}\nResponse:\n{}'.format(response.status_code, response.json()))
 
 
-def main():
-    print(sys.version)
-    print('File is: {}'.format(__name__))
+def run():
+    pass
 
 
 if __name__ == '__main__':
-    main()
+    print('Executing as main')
+    print('__name__ ==', __name__)
+    run()
+else:
+    print('Executing module: ', __name__)
