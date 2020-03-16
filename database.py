3d54396d0d6e39f16ef4eb66eab157a7498ed012
Martin K <mrtn.karlsson@gmail.com>
2020-03-16 10:48:47 +0100

    Merge branch 'master' of github.com:Ch4uwa/ex-app
    # Conflicts:
    #	database.py

--- database.py
+++ database.py
@@ -14,7 +14,17 @@ import sqlite3
 
 class DataBaseActions:
     def __init__(self):
-        super().__init__()
+        db_url = None
+        db_name = None
+
+        db_username = None
+        db_password = None
+
+    def db_connect(self):
+        pass
+
+    def db_create_queries(self):
+        pass
 
 
 if __name__ == '__main__':
--- request.py
+++ request.py
@@ -10,38 +10,44 @@ import sys
 # Todo Move .env file into db
 try:
     import requests
-
     from dotenv import load_dotenv
     load_dotenv()
+
 except ImportError as import_error:
-    print(import_error)
+    # Expected
+    print(import_error.__class__.__name__ + ': {}'.format(import_error))
+except Exception as imp_exception:
+    # Unexpected
+    print(imp_exception, False)
+    print(imp_exception.__class__.__name__ + ': {}'.format(imp_exception))
 
 
 # Class for handling weather API calls
-# Todo connect with selection of current location
+# Todo connect with select current location
 class Weather:
     def __init__(self):
         super().__init__()
         # 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}'
-        self.appid = os.getenv('WEATHER_API')
-        self.q = 'Stockholm'
+        self.api_key = os.getenv('WEATHER_API')
+        self.city = 'Stockholm'
         self.api_call = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(
-            self.q, self.appid)
+            self.city, self.api_key)
         print(self.api_call)
 
     def get_weather(self):
-
+        # Current weather Stockholm
         try:
             response = requests.get(self.api_call)
-        except Exception as e:
-            print('Exception:', e)
-        finally:
             if response.ok:
-                print('Status: {}\nResponse:\n{}'.format(
-                    response.status_code, response.text))
+                print('Status Code: {}\nResponse:\n{}'.format(response.status_code, response.json()))
+        except requests.exceptions as req_error:
+            print('Exception:', req_error)
+
+
+def main():
+    print(sys.version)
+    print('File is: {}'.format(__name__))
 
 
 if __name__ == '__main__':
-    print(__name__)
-    weather = Weather()
-    weather.get_weather()
+    main()
